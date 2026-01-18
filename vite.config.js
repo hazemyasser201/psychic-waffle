f0f9e91b6248131caf1372465a93160244e96680
Hazem Yasser ElSayed Hassan <Hazem.hassan04@eng-st.cu.edu.eg>
2026-01-18 22:37:40 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     port: 3000,
     strictPort: true,
-    allowedHosts: ['zuma.francecentral.cloudapp.azure.com'],
+    // allowedHosts: ['zuma.francecentral.cloudapp.azure.com'],
   },
-  base: '/psychic/'
+  // base: '/psychic/'
 })
